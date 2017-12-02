d0994705d6d89d306ad081ddf6b9721b11f49593
syedabdullahrahman <shawon120115@gmail.com>
2017-12-02 23:38:24 +0600

    Modified b=3500

--- swap.c
+++ swap.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
+void swap(int *a,int *b){
+	/* code */
+	int temp = *a;
+	*a=*b;
+	*b=temp;
+}
 int main()
 {
 		int a,b;
 		a=5;
-		b=3.5;
-		
+		b=3500;
+
 		//Adding swap functionality
-		
+		swap(&a,&b);
 		return 0;
 }
